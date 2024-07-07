491bd2d4744a8af54ac159125b249e30323422d6
FaridAliyev <faridaliyev407@gmail.com>
2024-07-07 18:44:04 -0400

    feat: test hooks

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = lazy(() => import('views/layout/dashboard'));
 
 const LoginPage = lazy(() => import('views/auth/login'));
 
-// remove lazy in the future
+// remove lazy loading in the future
 const HomePage = lazy(() => import('views/dashboard/home'));
 const RolesPage = lazy(() => import('views/dashboard/roles'));
 
--- src/views/dashboard/roles/roles.tsx
+++ src/views/dashboard/roles/roles.tsx
@@ -1,23 +1,11 @@
-import { Grid, Typography } from '@mui/material';
-import { PageHeader } from 'components/page-header';
-import { useTranslation } from 'react-i18next';
+import { Grid } from '@mui/material';
 
 import { Table } from './components/table';
 
 export const Roles = () => {
-	const { t } = useTranslation();
-
 	return (
 		<Grid container spacing={6}>
-			<Grid item xs={12}>
-				<PageHeader
-					title={
-						<Typography variant="h4" sx={{ mb: 4 }}>
-							{t('roles:pageTitle')}
-						</Typography>
-					}
-				/>
-			</Grid>
+			<Grid item xs={12}></Grid>
 			<Grid item xs={12}>
 				<Table />
 			</Grid>
